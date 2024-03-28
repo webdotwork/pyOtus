76c704b73b764f5fa03c5d22051837e244f85339
webdotwork <webdotwork@gmail.com>
2024-03-28 21:29:54 +0300

    refactor

--- src/figure/class_square.py
+++
@@ -1,8 +0,0 @@
-from class_rectangle import Rectangle
-
-
-class Square(Rectangle):
-    def __init__(self, side_a):
-        if side_a <= 0:
-            raise ValueError("нельзя создать квадрат")
-        super().__init__(side_a, side_a, "Square")
