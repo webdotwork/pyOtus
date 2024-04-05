caed17d01375128bdda647272cc3c9421f3e1f73
Roland Reagan <40559167+webdotwork@users.noreply.github.com>
2024-04-05 21:12:12 +0300

    Lesson4 (#8)
    * lesson4 solved
    
    * lesson4 solved
    
    * refactor
    
    * refactor
    
    * refactor
    
    ---------
    
    Co-authored-by: webdotwork <webdotwork@gmail.com>

---
+++ src/csv_reader.py
@@ -0,0 +1,15 @@
+import csv
+from csv import DictReader
+
+
+def csv_reader(csv_file_path: str) -> []:
+
+    out = list()
+
+    with open(csv_file_path, newline="") as f:
+        reader = DictReader(f)
+        # Итерируемся по данным делая из них словари
+        for row in reader:
+            out.append(row)
+
+    return out
