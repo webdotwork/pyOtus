7be0bf2e77176fad1c1289c233c3856228748a3f
webdotwork <webdotwork@gmail.com>
2024-07-28 12:02:01 +0300

    solved lesson8

--- page_objects/register_user_page.py
+++ page_objects/register_user_page.py
@@ -1,60 +1,105 @@
 from selenium.webdriver.common.by import By
-from helpers import random_string
+from helpers import random_string, random_email
 from page_objects.base_page import BasePage
-from helpers import random_email
+
 
 class RegisterUser(BasePage):
+    INPUT_FIRST_NAME = (By.CSS_SELECTOR, "input[id='input-firstname']")
+    INPUT_SECOND_NAME = (By.CSS_SELECTOR, "input[id='input-lastname']")
+    INPUT_EMAIL = (By.CSS_SELECTOR, "input[id='input-email']")
+    INPUT_PASSWD = (By.CSS_SELECTOR, "input[id='input-password']")
+    CHECK_BOX = (By.CSS_SELECTOR, "input[name='agree']")
+    SUBMIT = (By.CSS_SELECTOR, "button[type='submit']")
+    ACCEPT = (By.CSS_SELECTOR, "#content > div > a")
+    LOGOUT_BTN = (By.CSS_SELECTOR, "#column-right > div > a:nth-child(13)")
 
-    INPUT_FIRST_NAME = By.CSS_SELECTOR, "input[id='input-firstname']"
-    INPUT_SECOND_NAME = By.CSS_SELECTOR, "input[id='input-lastname']"
-    INPUT_EMAIL = By.CSS_SELECTOR, "input[id='input-email']"
-    INPUT_PASSWD = By.CSS_SELECTOR, "input[id='input-password']"
-    CHECK_BOX = By.CSS_SELECTOR, "input[name='agree']"
-    SUBMIT = By.CSS_SELECTOR, "button[type='submit']"
-    ACCEPT = By.CSS_SELECTOR, "#content > div > a"
-    LOGOUT_BTN = By.CSS_SELECTOR, "#column-right > div > a:nth-child(13)"
     FIRST_NAME = random_string()
     SECOND_NAME = random_string()
     EMAIL = random_email()
-    PASSWD = random_email()
-    SBM = By.CSS_SELECTOR, "#form-login > div.text-end > button"
-    # EMAIL_N = "testuser123@hmil.com"
-    # PASSWD_N = "Testuser@123"
+    PASSWD = random_string()
 
-    def get_inputs(self):
-        return self.get_elements(self.INPUT_FIRST_NAME)
+    def input_data(self, locator, value):
+        self.input_value(locator, value)
 
-    def input_data_first_name(self):
-        self.input_value(self.INPUT_FIRST_NAME, self.FIRST_NAME)
+    def fill_registration_form(self):
+        data = [
+            (self.INPUT_FIRST_NAME, self.FIRST_NAME),
+            (self.INPUT_SECOND_NAME, self.SECOND_NAME),
+            (self.INPUT_EMAIL, self.EMAIL),
+            (self.INPUT_PASSWD, self.PASSWD)
+        ]
+        for locator, value in data:
+            self.input_data(locator, value)
 
-    def input_data_second_name(self):
-        self.input_value(self.INPUT_SECOND_NAME, self.SECOND_NAME)
-
-    def input_data_email(self):
-        self.input_value(self.INPUT_EMAIL, self.EMAIL)
-
-
-    def input_data_passwd(self):
-        self.input_value(self.INPUT_PASSWD, self.PASSWD)
-
-
-    # def input_data_email_n(self):
-    #     text =
-    #     self.input_value(self.INPUT_PASSWD, text)
-
-    # def input_data_passwd_n(self, text):
-    #     self.input_value(self.INPUT_EMAIL, text)
     def click_check_box(self):
         self.click(self.CHECK_BOX)
+
     def submit(self):
         self.click(self.SUBMIT)
 
-    def sbm(self):
-        self.click(self.SBM)
-
     def accept(self):
-        self.click(self.SUBMIT)
+        self.click(self.ACCEPT)
 
     def logout(self):
         self.click(self.LOGOUT_BTN)
 
+# from selenium.webdriver.common.by import By
+# from helpers import random_string
+# from page_objects.base_page import BasePage
+# from helpers import random_email
+#
+# class RegisterUser(BasePage):
+#
+#     INPUT_FIRST_NAME = By.CSS_SELECTOR, "input[id='input-firstname']"
+#     INPUT_SECOND_NAME = By.CSS_SELECTOR, "input[id='input-lastname']"
+#     INPUT_EMAIL = By.CSS_SELECTOR, "input[id='input-email']"
+#     INPUT_PASSWD = By.CSS_SELECTOR, "input[id='input-password']"
+#     CHECK_BOX = By.CSS_SELECTOR, "input[name='agree']"
+#     SUBMIT = By.CSS_SELECTOR, "button[type='submit']"
+#     ACCEPT = By.CSS_SELECTOR, "#content > div > a"
+#     LOGOUT_BTN = By.CSS_SELECTOR, "#column-right > div > a:nth-child(13)"
+#     FIRST_NAME = random_string()
+#     SECOND_NAME = random_string()
+#     EMAIL = random_email()
+#     PASSWD = random_email()
+#     SBM = By.CSS_SELECTOR, "#form-login > div.text-end > button"
+#     # EMAIL_N = "testuser123@hmil.com"
+#     # PASSWD_N = "Testuser@123"
+#
+#     def get_inputs(self):
+#         return self.get_elements(self.INPUT_FIRST_NAME)
+#
+#     def input_data_first_name(self):
+#         self.input_value(self.INPUT_FIRST_NAME, self.FIRST_NAME)
+#
+#     def input_data_second_name(self):
+#         self.input_value(self.INPUT_SECOND_NAME, self.SECOND_NAME)
+#
+#     def input_data_email(self):
+#         self.input_value(self.INPUT_EMAIL, self.EMAIL)
+#
+#
+#     def input_data_passwd(self):
+#         self.input_value(self.INPUT_PASSWD, self.PASSWD)
+#
+#
+#     # def input_data_email_n(self):
+#     #     text =
+#     #     self.input_value(self.INPUT_PASSWD, text)
+#
+#     # def input_data_passwd_n(self, text):
+#     #     self.input_value(self.INPUT_EMAIL, text)
+#     def click_check_box(self):
+#         self.click(self.CHECK_BOX)
+#     def submit(self):
+#         self.click(self.SUBMIT)
+#
+#     def sbm(self):
+#         self.click(self.SBM)
+#
+#     def accept(self):
+#         self.click(self.SUBMIT)
+#
+#     def logout(self):
+#         self.click(self.LOGOUT_BTN)
+#
--- tests/test_curency_check.py
+++ tests/test_curency_check.py
@@ -1,19 +1,56 @@
+import pytest
 import random
 import time
 from selenium.webdriver.common.by import By
 from page_objects.base_page import BasePage
-def test_main_page_curency(browser):
-    curency_switch_list = BasePage(browser).get_elements((By.CSS_SELECTOR, "#form-currency > div > a"))
-    curency_switch_list[0].click()
-    random_what_ever = BasePage(browser).get_element((By.CSS_SELECTOR, f"#form-currency > div > ul > li:nth-child({random.randint(1, 3)}) > a"))
-    random_what_ever.click()
-    curent_curency = BasePage(browser).get_element((By.CSS_SELECTOR, "#form-currency > div > a > strong")) # Пауза для демонстрации
-    curency_check = BasePage(browser).get_elements((By.CSS_SELECTOR, ".price-new"))
-    for c in curency_check:
-        try:
-            curency = c.text
-            assert curent_curency.text in curency
-            print(f'curent curency: {curent_curency.text}\ncurency_check: {curency}')
-        except Exception as e:
-            print("Error:", e)
-    time.sleep(1)  # Пауза для демонстрации
+import logging
+
+# Configure logging
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('example.log')
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+@pytest.mark.usefixtures("browser")
+def test_main_page_currency(browser):
+    logger.info('====== Started: {} ======'.format(int(time.time())))
+    try:
+        logger.debug('Getting list of currency switch options')
+        base_page = BasePage(browser)
+        currency_switch_list = base_page.get_elements((By.CSS_SELECTOR, "#form-currency > div > a"))
+
+        logger.debug('Clicking on the first currency switch option')
+        currency_switch_list[0].click()
+
+        random_currency_selector = f"#form-currency > div > ul > li:nth-child({random.randint(1, 3)}) > a"
+        logger.debug(f'Randomly selecting a currency with selector: {random_currency_selector}')
+        random_what_ever = base_page.get_element((By.CSS_SELECTOR, random_currency_selector))
+
+        logger.debug('Clicking the randomly selected currency option')
+        random_what_ever.click()
+
+        logger.debug('Getting current currency symbol')
+        current_currency = base_page.get_element((By.CSS_SELECTOR, "#form-currency > div > a > strong"))
+
+        logger.debug('Getting list of new prices to check currency')
+        currency_check = base_page.get_elements((By.CSS_SELECTOR, ".price-new"))
+
+        for c in currency_check:
+            try:
+                currency = c.text
+                assert current_currency.text in currency, "Current currency not found in price"
+                logger.debug(f'Current currency: {current_currency.text}, Price checked: {currency}')
+            except Exception as e:
+                logger.error(f'Error checking currency in price: {e}')
+                raise
+
+        logger.info('====== Finished: {} ======'.format(int(time.time())))
+
+    except Exception as e:
+        logger.error('Error during test execution: %s', e)
+        raise
+    finally:
+        time.sleep(1)  # Pause for demonstration
+
