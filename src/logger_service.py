9971b5d4c92969bc53ea1915f2246d733fa700f0
Roland Reagan <40559167+webdotwork@users.noreply.github.com>
2024-07-28 21:51:50 +0300

    solved lesson8 (#12)
    Co-authored-by: webdotwork <webdotwork@gmail.com>

---
+++ src/logger_service.py
@@ -0,0 +1,10 @@
+import logging
+
+def logger_service():
+    logger = logging.getLogger(__name__)
+
+    file_handler = logging.FileHandler('example.log')
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+    logger.setLevel(logging.DEBUG)
+
--- tests/test_add_new_user.py
+++ tests/test_add_new_user.py
@@ -1,20 +1,56 @@
+import pytest
 from page_objects.register_user_page import RegisterUser
 from page_objects.main_page import MainPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import logging
+import time
 
+# Configure logging
+logger = logging.getLogger(__name__)
+file_handler = logging.FileHandler('example.log')
+file_handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+logger.addHandler(file_handler)
+logger.setLevel(logging.DEBUG)
+
+
+@pytest.mark.usefixtures("browser")
 def test_add_new_user(browser):
-    MainPage(browser).click_my_account()
-    MainPage(browser).click_register_user()
-    RegisterUser(browser).input_data_first_name()
-    RegisterUser(browser).input_data_second_name()
-    RegisterUser(browser).input_data_email()
-    RegisterUser(browser).input_data_passwd()
-    RegisterUser(browser).click_check_box()
-    RegisterUser(browser).submit()
-    RegisterUser(browser).accept()
-    RegisterUser(browser).logout()
-    check = WebDriverWait(browser, 2).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#column-right > div > a:nth-child(4)")))
-    assert check.text == 'My Account', "тест провален DUDE!!"
+    logger.info('====== Started: {} ======'.format(int(time.time())))
+    try:
+        logger.debug('Navigating to Main Page')
+        main_page = MainPage(browser)
+
+        logger.debug('Clicking My Account')
+        main_page.click((By.CSS_SELECTOR, "a[title='My Account']"))
+
+        logger.debug('Clicking Register User')
+        main_page.click((By.CSS_SELECTOR, "a[title='Register']"))
+
+        logger.debug('Filling registration form')
+        register_user = RegisterUser(browser)
+        register_user.fill_registration_form()
+
+        logger.debug('Agreeing to terms and conditions')
+        register_user.agree_to_terms()
+
+        logger.debug('Submitting registration form')
+        register_user.submit_registration()
+
+        logger.debug('Accepting registration')
+        register_user.accept_registration()
+
+        logger.debug('Logging out new user')
+        register_user.logout()
+
+        logger.debug('Checking if "My Account" link is present')
+        check = WebDriverWait(browser, 2).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "#column-right > div > a:nth-child(4)")))
+        assert check.text == 'My Account', "тест провален DUDE!!"
+
+        logger.info('====== Finished: {} ======'.format(int(time.time())))
+
+    except Exception as e:
+        logger.error('Error during test execution: %s', e)
+        raise
