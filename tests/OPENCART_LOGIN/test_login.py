f52510785ec4cc1a24a6cb3a4fc65222cf7a8fb5
webdotwork <webdotwork@gmail.com>
2024-09-08 12:56:01 +0300

    solved project

--- tests/OPENCART_LOGIN/test_login.py
+++ tests/OPENCART_LOGIN/test_login.py
@@ -73,14 +73,13 @@ def test_login(browser):
             logger.debug('Submitting login form')
             browser.find_element(By.CSS_SELECTOR, "button[type='submit']").click()
             take_screenshot(browser, 'Login Form Submitted')
-            time.sleep(3)
-        # with allure.step('Checking if "My Account" link is present'):
-        #     logger.debug('Checking if "My Account" link is present')
-        #     check = WebDriverWait(browser, 4).until(
-        #         EC.presence_of_element_located((By.CSS_SELECTOR, "#column-right > div > a:nth-child(1)")))
-        #     time.sleep(2)
-        #     # assert check.text == 'My Account', "тест провален DUDE!!"
-        #     take_screenshot(browser, 'My Account Link Present')
+        with allure.step('Checking if "My Account" link is present'):
+            logger.debug('Checking if "My Account" link is present')
+            check = WebDriverWait(browser, 4).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "#column-right > div > a:nth-child(1)")))
+            time.sleep(2)
+            assert check.text == 'My Account', "тест провален DUDE!!"
+            take_screenshot(browser, 'My Account Link Present')
 
         logger.info('====== Finished: {} ======'.format(int(time.time())))
 
